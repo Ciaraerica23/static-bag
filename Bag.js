70551948102377a6599d72407a1460c7d856dfe2
Ciaraerica23 <ciaraestafford@gmail.com>
2023-02-27 09:20:14 -0500

    first

--- Bag.js
+++ Bag.js
@@ -1,10 +1,15 @@
 class Bag {
+    static maxWeight=23
     constructor(weight) {
         if(!weight){
             throw new Error('bag needs weight')
         } 
         this.weight = weight
+        if(this.weight>Bag.maxWeight){
+            throw new Error('max weight reached')
+        }
     }
+    
 }
 
 module.exports = Bag
